ec9442f17770d36ba943f94465a02242489ce73d
MinLL <fdsatrashasdf@gmail.com>
2024-10-20 22:29:06 -0700

    Add safe dir config to updater

--- api/update.php
+++ api/update.php
@@ -11,8 +11,22 @@ $repoDir = __DIR__ . '/../';  // Adjust the path based on your folder structure
 // Change to the repository directory
 chdir($repoDir);
 
+$safeDirCmd = "HOME=/tmp git config --global --add safe.directory /var/www/html/HerikaServer/ext/minai_plugin";
+$output = [];
+$returnVar = 0;
+exec($safeDirCmd, $output, $returnVar);
+
+if ($returnVar !== 0) {
+    echo json_encode([
+        'status' => 'error',
+        'message' => "Failed to checkout branch $branch",
+        'output' => $output
+    ]);
+    exit;
+}
+
 // Checkout the desired branch
-$checkoutCmd = "git checkout $branch 2>&1";
+$checkoutCmd = "HOME=/tmp git checkout $branch 2>&1";
 $output = [];
 $returnVar = 0;
 exec($checkoutCmd, $output, $returnVar);
@@ -27,7 +41,7 @@ if ($returnVar !== 0) {
 }
 
 // Run the git pull command
-$pullCmd = 'git pull 2>&1';
+$pullCmd = 'HOME=/tmp git pull 2>&1';
 exec($pullCmd, $output, $returnVar);
 
 if ($returnVar === 0) {
